e02c2c1217c6463e4d32c0723f14bf4e506bd4f9
eduardo severeyn <eduardosevereyn@eduardos-air.myfiosgateway.com>
2021-12-05 20:29:04 -0500

    db

--- app.js
+++ app.js
@@ -4,32 +4,25 @@ app =  express();
 require('dotenv').config()
 var sslRedirect  = require("heroku-ssl-redirect").default;
 var compression = require('compression');
-//////////////////////////////////
-/////////////database/////////////
-const { Pool } = require('pg');
-const pool = new Pool({
+const { Client } = require('pg');
+const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
-
+})
 
 //MIDDLEWARE
 app.set("port",process.env.PORT || 3000);
 app.set("view engine", "ejs");
-app.use(express.json());
 app.use(sslRedirect());
 app.disable('x-powered-by');
 app.use(compression());
 app.use(express.static("public"));
+app.use(express.json());
 app.use(
     express.urlencoded({
         extended:false
     })
 );
-
+app.use
 const errorController  = require('./controllers/errorController');
 const middleware = require('./controllers/middleware')
 
@@ -38,17 +31,28 @@ const middleware = require('./controllers/middleware')
 app.get('/',(req,res,next) => {
     res.render('test')
 });
-app.get('/mydb', async (req, res) => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query('SELECT * FROM test_table');
-    const results = { 'results': (result) ? result.rows : null};
-    res.render('pages/db', results );
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
+app.post('/thanks', async (req, res) => {
+    
+    data = {
+            name : req.body.name,
+            email : req.body.email,
+            service: req.body.service,
+            message: req.body.message};
+    
+    //const text =`INSERT INTO customers(name,email,service,message) VALUES(${data.name}, ${data.email}, ${data.service}, ${data.message}) RETURNING *`
+    const values =[data.name, data.email, data.service, data.message];
+    customers = 'customers'
+    client.connect()
+      try {
+        const res = await client.query(`SELECT * FROM ${customers}`);
+        console.log(res.row[1])
+        client.end()
+    
+      }catch (err) {
+        console.log(err.stack)
+        client.end()
+      }
+  res.render('thanks')
 })
 app.get('/contact',(req,res) => {
     res.render('contact')
