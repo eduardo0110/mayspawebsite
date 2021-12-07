25c46121eaef52e78b0617fd2b4345e8fb4588ac
eduardo severeyn <eduardosevereyn@eduardos-air.myfiosgateway.com>
2021-12-07 10:10:57 -0500

    ddda

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ require('dotenv').config()
 var sslRedirect  = require("heroku-ssl-redirect").default;
 var compression = require('compression');
 const { Pool } = require('pg');
-var connectionString = "postgres://postgres@localhost:5432/eduardosevereyn";
+var connectionString = "postgres://mdocxafgwhicga:27880ae1069e486dbc293489547bca4483ba232f83a9ea2f092c0d9a8287e94e@ec2-18-210-159-154.compute-1.amazonaws.com:5432/d103b4gar59tdk";
 const pool = new Pool ({
-    connectionString:connectionString
+    connectionString:connectionString,
+    ssl:{
+        rejectUnauthorized:false
+    }
 });
 
 
@@ -26,9 +29,6 @@ app.use(
 );
 app.use
 const errorController  = require('./controllers/errorController');
-//const middleware = require('./controllers/middleware')
-
-//ROUTES
 
 app.get('/',(req,res,next) => {
     res.render('test')
@@ -41,15 +41,11 @@ app.post('/thanks', async (req, res) => {
             service: req.body.service,
             message: req.body.message};
     
-    const text ='INSERT INTO nada(name,email,services,message) VALUES ($1, $2, $3, $4)';
+    const text ='INSERT INTO contactinfo(name,email,services,message) VALUES ($1, $2, $3, $4)';
     const values =[data.name, data.email, data.service, data.message];
-    
-    
-    
-      try {
+    try {
         const res = await pool.query(text,values);
-        
-        
+        console.log(res.row[0])
     
       }catch (err) {
         console.log(err.stack)
@@ -72,7 +68,6 @@ app.get("/about" , (req,res) => {
 app.get('/maysspabeauty.com/contact/*' , (req , res) => {
     res.render('contact')
 })
-
 
 app.use(errorController.pageNotFoundError);
 app.use(errorController.internalServerError)
